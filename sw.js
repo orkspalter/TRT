a20fab7e3d630bebdc535b9bb60fe91f248f64f4
orkspalter <timm.schele@mailbox.org>
2026-02-01 13:43:45 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,30 @@
-self.addEventListener("install", event => {
+const CACHE_NAME = "timm-recipes-cache-v2";
+const OFFLINE_URLS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("weekly-recipe-cache-v1").then(cache =>
-      cache.addAll(["./", "./index.html", "./manifest.json"])
-    )
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(OFFLINE_URLS))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
