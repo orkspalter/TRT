34ea94d5cf1595fc8df8da5e0efe84ae0506164b
orkspalter <timm.schele@mailbox.org>
2026-03-28 11:27:38 +0100

    <3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "timm-recipes-cache-v19"; // <- bei Updates hochzählen
+const CACHE_NAME = "timm-recipes-cache-v20"; // <- bei Updates hochzählen
 const OFFLINE_URLS = [
   "./",
   "./index.html",
